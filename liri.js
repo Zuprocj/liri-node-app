b75c6c141fd5e5a38096e30264e75be9d5bfefb8
zuprocj <jmdcorpuz@gmail.com>
2019-09-30 15:06:07 -0700

    added spotify functions

--- liri.js
+++ liri.js
@@ -2,3 +2,27 @@ require("dotenv").congig();
 
 var keys = require("./keys.js");
 var spotify = new spotify(keys.spotify);
+
+var getArtistsNames = function(artist) {
+    return artist.name;
+}
+var getMeSpotify = function(songName) {
+
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if ( err ) {
+            console.log('Error occured: ' + err);
+            return;
+        }
+    
+    var songs = data.tracks.tems;
+    for(var i=0, i<songs.length; i++) {
+        console.log(i);
+        console.log('artist(s): ' + songs[i].artists.map(
+            getArtistsNames));
+        console.log('song name: ' + songs[i].name);
+        console.log('preview song: ' + songs[i].preview.url);
+        console.log('album: ' + songs[i].album.name);
+        console.log ('------------------------------------');
+    }
+    });
+}
