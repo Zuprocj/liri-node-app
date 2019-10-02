7982c16f20d7190f628a1a1c68b975740c90f7d3
zuprocj <jmdcorpuz@gmail.com>
2019-10-02 11:23:28 -0700

    added movie function

--- liri.js
+++ liri.js
@@ -37,25 +37,59 @@ function userCommand(userInput, userQuery) {
     }
 }
 
+userCommand(userInput, userQuery);
+
+function concertThis() {
+    console.log(`\n - - - - -\n\nSEARCHING FOR...${userQuery}'s next show...`);
+    request("https://rest.bandsintown.com/artists/" + userQuery + "/events?app_id=" + bandsintown, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            let userBand = JSON.parse(body);
+            if (userBand.length > 0) {
+                for (i = 0; i < 1; i++) {
+                    console.log(`\nIs this is what you are looking for...\n\nArtist: ${userBand[i].lineup[0]} \nVenue: ${userBand[i].venue.name}
+                    \nVenue Location: ${userBand[i].venue.latitude},${userBand[i].venue.longitude}\nVenue City: ${userBand[i].venue.city}, ${userBand[i].venue.country}`)
+                    let concertDate = moment(userBand[i].datetime).format("MM/DD/YYYY hh:00 A");
+                    console.log(`Date and Time: ${concertDate}\n\n- - - - -`);
+                };
+            } else {
+                console.log('Band or concert not found!');
+            };
+        };
+    });
+};
+
+
 function spotifyThisSong() {
     console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
-
-    // IF USER QUERY NOT FOUND, PASS VALUE OF "ACE OF BASE" 
     if (!userQuery) {
         userQuery = "the sign ace of base"
     };
-
     spotify.search({ type: 'track', query: songName }, function(err, data) {
         if ( err ) {
             console.log('Error occured: ' + err);
             return;
         }
-      
       let spotifyArr = data.tracks.items;
-
       for (i = 0; i < spotifyArr.length; i++) {
-          console.log(`\nIs this is what you are looking for
-          ...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}\nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
+          console.log(`\nIs this is what you are looking for...\n\nArtist: ${data.tracks.items[i].album.artists[0].name} \nSong: ${data.tracks.items[i].name}
+          \nAlbum: ${data.tracks.items[i].album.name}\nSpotify link: ${data.tracks.items[i].external_urls.spotify}\n\n - - - - -`)
       };
   });
 }
+
+function movieThis() {
+    console.log(`\n - - - - -\n\nSEARCHING FOR..."${userQuery}"`);
+    if (!userQuery) {
+        userQuery = "mr nobody";
+    };
+    request("http://www.omdbapi.com/?t=" + userQuery + "&apikey=86fe999c", function (error, response, body) {
+        let userMovie = JSON.parse(body);
+        let ratingsArr = userMovie.Ratings;
+        if (ratingsArr.length > 2) {}
+        if (!error && response.statusCode === 200) {
+            console.log(`\nBA DA BOP!  That's for you...\n\nTitle: ${userMovie.Title}\nCast: ${userMovie.Actors}\nReleased: ${userMovie.Year}\nIMDb Rating: ${userMovie.imdbRating}\nRotten Tomatoes Rating: ${userMovie.Ratings[1].Value}\nCountry: ${userMovie.Country}\nLanguage: ${userMovie.Language}\nPlot: ${userMovie.Plot}\n\n- - - - -`)
+        } else {
+            return console.log("Movie able to be found. Error:" + error)
+        };
+    })
+};
